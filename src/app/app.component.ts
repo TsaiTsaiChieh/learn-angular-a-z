ccefe3fce1301cecec6d86663fd923591ebf1947
Tsai-Chieh <d0177381@gmail.com>
2023-12-17 21:30:36 +0800

    :sparkles: 2.2 Creating a Data Model

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { WishItem } from './shared/models/wishItem';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'wishlist';
+  items = [
+    new WishItem('To Learn Angular'),
+    new WishItem('Get Coffee', true),
+    new WishItem('Find grass that cuts itself'),
+  ];
 }
---
+++ src/app/shared/models/wishItem.ts
@@ -0,0 +1,3 @@
+export class WishItem {
+  constructor(public wishText: String, public isCompleted: boolean = false) { }
+}
