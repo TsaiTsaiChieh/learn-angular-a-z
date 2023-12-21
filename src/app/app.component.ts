4779e61753b8e3feeaebf46e6230b00af7799191
Tsai-Chieh <d0177381@gmail.com>
2023-12-21 22:19:32 +0800

    :sparkles: Listening for Changes to ngModel Values

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,13 +13,29 @@ export class AppComponent {
     new WishItem('Get Coffee', true),
     new WishItem('Find grass that cuts itself'),
   ];
+
+  listFilter: string = '0';
   newWishItem = '';
+  visibleItems: WishItem[] = this.items;
+
   addNewWish() {
     // add wish to items array
     this.items.push(new WishItem(this.newWishItem));
     // clear the textbox
     this.newWishItem = '';
   }
+
+  filterChange(value: any) {
+    console.log(value);
+    if (value === '0') {
+      this.visibleItems = this.items;
+    } else if (value === '1') {
+      this.visibleItems = this.items.filter((ele) => !ele.isCompleted);
+    } else if (value === '2') {
+      this.visibleItems = this.items.filter((ele) => ele.isCompleted);
+    }
+  }
+
   toggleItem(item: WishItem) {
     item.isCompleted = !item.isCompleted;
     console.log(item);
